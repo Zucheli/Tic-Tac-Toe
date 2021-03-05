e010e9f1e851a31101daf1378d178a138926a143
Mateus Zucheli <mateuszucheli@hotmail.com>
2021-03-05 15:24:37 -0300

    Site Working

---
+++ js/interface.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    square.addEventListener("click", handleClick);
+  });
+});
+
+function handleClick(event) {
+  let square = event.target;
+  let position = square.id;
+
+  if (handleMove(position)) {
+    setTimeout(() => {
+      alert("Game Over - Player " + (playerTime + 1) + " Wins");
+    }, 10);
+  }
+  updateSquare(position);
+}
+
+function updateSquare(position) {
+  let square = document.getElementById(position.toString());
+  let symbol = board[position];
+  square.innerHTML = `<div class='${symbol}'></div>`;
+}
+
+function updateSquares() {
+  let squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    let position = square.id;
+    let symbol = board[position];
+
+    if (symbol != "") {
+      square.innerHTML = `<div class='${symbol}'></div>`;
+    }
+  });
+}
